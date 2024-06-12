d77c742042461a36add09367a10eca105037ba9f
Robin Bramley <robin@rbramley.com>
2024-06-12 11:20:21 +0100

    53: Use an alternative petapico API endpoint

--- __tests__/fsr.test.js
+++ __tests__/fsr.test.js
@@ -4,47 +4,70 @@
  */
 
 // Prepare the mock expectations
-const mockBody = `<!DOCTYPE html>
-<html>
-<head>
-<title>HTML5 table</title>
-</head>
-<body>
-<div class="container-fluid">
-<table class="table table-striped table-sm table-borderless">
-  <tr>
-    <th>np</th>
-    <th>label</th>
-    <th>types</th>
-    <th>date</th>
-  </tr>
-  <tr>
-    <td><a href="http://purl.org/np/RAx">http://purl.org/np/RAx</a></td>
-    <td><pre>Test ROHub</pre></td>
-    <td><pre>Registry</pre></td>
-    <td><pre>Registry</pre></td>
-    <td><pre>2023-11-05T09:55:32Z</pre></td>
-  </tr>
-  <tr>
-    <td><a href="http://purl.org/np/RAS">http://purl.org/np/RAS</a></td>
-    <td><pre>Test WDCC </pre></td>
-    <td><pre>FAIR-Practice</pre></td>
-    <td><pre>2023-10-22T21:43:36Z</pre></td>
-  </tr>
-</table></div>
-</body>
-</html>`
+const mockBody = 
+`{
+  "head" : {
+    "vars" : [
+      "np",
+      "label",
+      "types",
+      "date"
+    ]
+  },
+  "results" : {
+    "bindings" : [
+      {
+        "date" : {
+          "datatype" : "http://www.w3.org/2001/XMLSchema#dateTime",
+          "type" : "literal",
+          "value" : "2024-06-11T11:08:35.842Z"
+        },
+        "np" : {
+          "type" : "uri",
+          "value" : "https://w3id.org/np/RAGD"
+        },
+        "label" : {
+          "type" : "literal",
+          "value" : "Research object management platform"
+        },
+        "types" : {
+          "type" : "literal",
+          "value" : "Registry"
+        }
+      },
+      {
+        "date" : {
+          "datatype" : "http://www.w3.org/2001/XMLSchema#dateTime",
+          "type" : "literal",
+          "value" : "2024-06-11T11:04:08.237Z"
+        },
+        "np" : {
+          "type" : "uri",
+          "value" : "https://w3id.org/np/RAcs"
+        },
+        "label" : {
+          "type" : "literal",
+          "value" : "FAIR Data Station"
+        },
+        "types" : {
+          "type" : "literal",
+          "value" : "Registry, Fake"
+        }
+      }
+    ]
+  }
+}`
 
 const expectedRows = [
   {
-    np: 'http://purl.org/np/RAx',
+    np: 'https://w3id.org/np/RAGD',
     labels: ['Registry'],
-    title: 'Test ROHub'
+    title: 'Research object management platform'
   },
   {
-    np: 'http://purl.org/np/RAS',
-    labels: ['FAIR-Practice'],
-    title: 'Test WDCC '
+    np: 'https://w3id.org/np/RAcs',
+    labels: ['Registry', 'Fake'],
+    title: 'FAIR Data Station'
   }
 ]
 
@@ -75,7 +98,7 @@ describe('action', () => {
   })
 
   // Positive case
-  it('Gets rows from HTML5', async () => {
+  it('Gets rows from JSON', async () => {
     const rows = await fsr.fetchFSRs()
 
     // assertions
